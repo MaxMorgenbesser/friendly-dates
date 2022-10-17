a7e7af3f3c3e6d4a8e75b5db2b8dc6853bb3c57c
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-10-17 12:17:32 -0400

    updated api with sms route

--- backend/functions/src/services.js
+++ backend/functions/src/services.js
@@ -12,18 +12,39 @@ export async function verifynum (req,res){
         res.status(200).send({"error":"number must be at least 10 digits"})
         return
     }
-
+let pin = ''
+let rnum = ''
     const collection = DbConnect()
-    const client = new twilio(accountSID,authToken);
+    for (let i = 1; i<5; i++){
+        rnum = Math.floor(Math.random(0) * 10)
+        pin += rnum
+        // console.log(rstring)
+      }
 
+const client = new twilio(accountSID,authToken);
+const user = await collection.findOne({num:number})
+if (user){
+    console.log("user already is in the db")
     client.messages.create({
         to:number,
         from:twilionumber,
-        body:"test" 
+        body:"Welcome back to friendly dating, here is your pin: " + pin 
     })
-    
-    console.log(number)
-    await collection.insertOne({num:number})
-    res.send({number:number})
+    res.send({"welcome back":true})
+} else {
+    // console.log(number)
+    client.messages.create({
+        to:number,
+        from:twilionumber,
+        body:"Welcome to friendly dating, here is your pin: " + pin 
+    })
+   const newuser =  await collection.insertOne({num:number, pin:pin})
+   const updateduser = await collection.findOneAndUpdate({pin:pin}, {
+    $set:{uid:newuser.insertedId.toString()
 
+    }
+   })
+   let updatevalue = updateduser.value
+    res.send({updatevalue})
+}
 }
