ecfa48c26d30826fc9d86df3ee84756a6f37ef7c
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-10-25 21:39:58 -0400

    added some styles, needs blob correctly

--- friendlyfrontend/Components/Home.js
+++ friendlyfrontend/Components/Home.js
@@ -1,23 +1,25 @@
-import { Button, Text, TextInput } from "react-native";
+import { Button, Text, TextInput, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useEffect, useState, useContext } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import jwtDecode from "jwt-decode";
 import { data } from "../App";
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
 
 export default function Home() {
   const navigation = useNavigation();
   const [number, setNumber] = useState("");
   const [error, setError] = useState("");
   // const [user, setUser] = useState("");
-  const { setUid, setTempToken, tempToken , user, setUser } = useContext(data);
+  const { setUid, setTempToken, tempToken, user, setUser } = useContext(data);
 
-  useEffect(()=>{
-   if (user){
-    navigation.navigate("pin")
-   }
-  },[user,setUser])
+  useEffect(() => {
+    if (user) {
+      navigation.navigate("pin");
+    }
+  }, [user, setUser]);
 
   const handlesubmit = async () => {
     fetch("https://friendlydatesbackend.web.app/users/verifynum", {
@@ -34,8 +36,8 @@ export default function Home() {
           setError(data.error);
         }
         if (data.token) {
-           setTempToken(data.token)
-          setUser(jwtDecode(data.token))
+          setTempToken(data.token);
+          setUser(jwtDecode(data.token));
         }
       })
       .catch((err) => console.log(err));
@@ -46,10 +48,11 @@ export default function Home() {
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
+        backgroundColor: "grey",
+        height: "100%",
       }}
     >
-      <Text>Home page</Text>
-
+      {/* <KeyboardAwareScrollView> */}
       {/* {user && console.log(user)} */}
       <Button
         title="go to Add user"
@@ -63,24 +66,70 @@ export default function Home() {
         title="go to App"
         onPress={() => navigation.navigate("App")}
       ></Button>
-
+      <View
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          borderColor: "",
+          borderWidth: "2",
+          fontWeight: "bold",
+          backgroundColor: "white",
+          margin: 75,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 30,
+            margin: 10,
+            fontFamily: "Academy Engraved LET",
+            color: "blue",
+          }}
+        >
+          Friendly Dating
+        </Text>
+        <Text
+          style={{
+            fontSize: 25,
+            fontFamily: "Academy Engraved LET",
+            color: "blue",
+          }}
+        >
+          Be Kind
+        </Text>
+      </View>
+      <Text
+      style={{
+        fontSize:20,
+        paddingHorizontal:50,
+        display:"flex",
+        alignItems:"center",
+        fontFamily: "Academy Engraved LET",
+        color:""
+        
+      }}
+      >
+        Please enter your phone number to sign up or login
+      </Text>
       <TextInput
+        placeholder="phone number"
         value={number}
         onChangeText={setNumber}
         keyboardType="numeric"
         style={{
           height: 40,
-          color:"black",
-         fontWeight:"bold",
+          color: "black",
+          fontWeight: "bold",
           borderWidth: 1,
           padding: 10,
-          margin:"8%",
+          margin: "8%",
           width: "40%",
           backgroundColor: "white",
         }}
       />
       {error && <Text>{error}</Text>}
       <Button title="submit number" onPress={() => handlesubmit()}></Button>
+      {/* </KeyboardAwareScrollView> */}
     </SafeAreaView>
   );
 }
--- friendlyfrontend/Components/MyProfile.js
+++ friendlyfrontend/Components/MyProfile.js
@@ -1,42 +1,101 @@
-import { View, Text, SafeAreaView ,Button,TouchableOpacity, Dimensions, Image } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Button,
+  TouchableOpacity,
+  Dimensions,
+  Image,
+} from "react-native";
+// import {storageConnect} from '../../backend/functions/dbconnect.js'
+import * as ImageManipulator from "expo-image-manipulator";
+// import RNFS from 'react-native-fs'
+// import storage from '@react-native-firebase/storage'
 import { data } from "../App";
 import { useContext, useEffect, useState } from "react";
 import jwtDecode from "jwt-decode";
-import * as ImagePicker from 'expo-image-picker';
-import {Camera, CameraType} from 'expo-camera'
-
+import * as ImagePicker from "expo-image-picker";
+import { Camera, CameraType } from "expo-camera";
+// import { getStorage } from "./Firebase";
 
 export default function MyProfile() {
   const [camera, setCamera] = useState(null);
+  const [fileBase64, setFileBase64] = useState(null);
   const { token } = useContext(data);
   const [user, setUser] = useState();
   const [type, setType] = useState(CameraType.back);
   const [photo, setPhoto] = useState();
+  const [blob, setBlob] = useState();
   const [showCamera, SetShowCamera] = useState(false);
 
+  useEffect(() => {
+    if (photo) {
+      // console.log(photo)
+      setBlob(getBlob(photo));
+      //  console.log(getBlob(photo));
+    }
+  }, [photo, setPhoto]);
+
+  const getBlob = async (fileUri) => {
+    // console.log(fileUri);
+
+    const resizedPhoto = await ImageManipulator.manipulateAsync(
+      fileUri,
+      [{ resize: { width: 400 } }], // resize to width of 300 and preserve aspect ratio
+      { compress: 0.6, format: "jpeg" }
+    );
+    console.log(resizedPhoto);
+    const resp = await fetch(resizedPhoto.uri);
+    const blob = await resp.blob()
+    return blob;
+  };
+
+
+  const submitPhoto = async () => {
+// const gc = await storageConnect()
+     setBlob( await getBlob(photo))
+
+
+    fetch(`https://friendlydatesbackend.web.app/users/updateuser/${user.uid}`, {
+      method: "PUT",
+      headers: {
+        Authorization: token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ photo: blob}), 
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setPhoto(null);
+        setBlob(null);
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  };
+
   const startCamera = async () => {
-  const {status} = await Camera.requestCameraPermissionsAsync()
- if(status === 'granted'){
-    SetShowCamera(!showCamera)
- }else{
-   Alert.alert("Access denied")
- }
-}
+    const { status } = await Camera.requestCameraPermissionsAsync();
+    if (status === "granted") {
+      SetShowCamera(!showCamera);
+    } else {
+      Alert.alert("Access denied");
+    }
+  };
 
+  const takePicture = async () => {
+    if (camera) {
+      const data = await camera.takePictureAsync();
+      // console.log(" --- DATA ---->> ", data);
+      setPhoto(data.uri);
+      SetShowCamera(false);
+    }
+  };
 
-const takePicture = async () => {
-  if (camera) {
-    const data = await camera.takePictureAsync();
-    console.log(data.uri);
-    setPhoto(data.uri);
-    SetShowCamera(false)
+  function toggleCameraType() {
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back
+    );
   }
-};
-
-
-function toggleCameraType() {
-  setType((current) => (current === CameraType.back ? CameraType.front : CameraType.back));
-}
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
@@ -47,24 +106,19 @@ function toggleCameraType() {
       quality: 1,
     });
 
-    console.log(result);
+    // console.log(result);
 
     if (!result.cancelled) {
       setPhoto(result.uri);
     }
   };
 
-
-
-
-
   useEffect(() => {
     if (token) {
-      console.log(token);
-      setUser(jwtDecode(token))
+      // console.log(token);
+      setUser(jwtDecode(token));
     }
   }, [token]);
-
 
   return (
     <SafeAreaView
@@ -77,8 +131,7 @@ function toggleCameraType() {
       }}
     >
       {photo && console.log(photo)}
-      {user && console.log(user)}
-      {token && console.log(token)}
+      {blob && console.log(blob)}
       <View>
         <Text>My profile is going here</Text>
       </View>
@@ -86,51 +139,68 @@ function toggleCameraType() {
       {user && (
         <View>
           <Text>welcome {user.user.firstName}</Text>
-         
-          
-
-          
-        
         </View>
       )}
-     { !photo && !showCamera && <Button title="Choose Photo" onPress={pickImage} />}
-      {photo && <><Image source = {{uri:photo}} style={{ height: 200, margin: 10, width: 200}} resizeMode="contain"/>
-      <Button title="use a different photo" onPress={()=> setPhoto(null)} ></Button>
-      <Button title="Submit Photo"  ></Button>
-      </>}
+      {!photo && !showCamera && (
+        <Button title="Choose Photo" onPress={pickImage} />
+      )}
+      {photo && (
+        <>
+          <Image
+            source={{ uri: photo }}
+            style={{ height: 200, margin: 10, width: 200 }}
+            resizeMode="contain"
+          />
+          <Button
+            title="use a different photo"
+            onPress={() => setPhoto(null)}
+          ></Button>
+          <Button title="Submit Photo" onPress={submitPhoto}></Button>
+        </>
+      )}
 
-    
-      {!photo && !showCamera &&  <Button title = "Take photo instead" onPress={()=>{
-          setPhoto(null)
-          startCamera()}}></Button>
-      }
-      {showCamera && <Camera type={type} 
-      ref={(ref) => setCamera(ref)}
-      style={{flex: 1,width:"100%"}}
-      ><View>
-        
-        <TouchableOpacity style={{
-        backgroundColor: "white",
-        width: 50,
-        height: 50,
-        borderRadius: 50,
-      }}
-      onPress={takePicture}/>
-        <TouchableOpacity
-        onPress={toggleCameraType}
-        style={
-         {
-          backgroundColor: "white",
-          width: 50,
-          height: 50,
-          borderRadius: 50,
-        }
-        
-      }/>
-      
-      <Button title = "cancel" onPress={()=>SetShowCamera(false)}></Button>
-      </View>
-        </Camera>}
+      {!photo && !showCamera && (
+        <Button
+          title="Take photo instead"
+          onPress={() => {
+            setPhoto(null);
+            startCamera();
+          }}
+        ></Button>
+      )}
+      {showCamera && (
+        <Camera
+          type={type}
+          ref={(ref) => setCamera(ref)}
+          style={{ flex: 1, width: "100%" }}
+        >
+          <View>
+            <TouchableOpacity
+              style={{
+                backgroundColor: "white",
+                width: 50,
+                height: 50,
+                borderRadius: 50,
+              }}
+              onPress={takePicture}
+            />
+            <TouchableOpacity
+              onPress={toggleCameraType}
+              style={{
+                backgroundColor: "white",
+                width: 50,
+                height: 50,
+                borderRadius: 50,
+              }}
+            />
+
+            <Button
+              title="cancel"
+              onPress={() => SetShowCamera(false)}
+            ></Button>
+          </View>
+        </Camera>
+      )}
     </SafeAreaView>
   );
 }
